cc1f26ecab89a3aa8517698849ba13bbb262f9dc
DennisGermishuys <dennisgermishuys20@gmail.com>
2023-04-16 17:28:13 +0200

    firebase initialization

--- src/pages/technologies/[name].tsx
+++ src/pages/technologies/[name].tsx
@@ -1,35 +1,44 @@
-import { PrismaClient } from "@prisma/client";
 import { GetServerSideProps } from "next";
 import styles from "../../styles/tech.module.css";
 import { Variants, motion } from "framer-motion";
-import Image from "next/image";
-
-const prisma = new PrismaClient();
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore, getDocs, onSnapshot, query, where, doc } from "firebase/firestore";
 
 export const getServerSideProps: GetServerSideProps = async ({
   resolvedUrl,
 }) => {
   const dbParam: string = resolvedUrl.split("/").pop()!;
 
-  const details = await prisma.technology.findFirst({
-    where: {
-      name: dbParam,
-    },
-  });
+  const firebaseConfig = {
+  apiKey: "AIzaSyDhsJ5qCZXLeJS57PAVEbOi5_7D-T-6_ls",
+  authDomain: "nextjs-database-60f90.firebaseapp.com",
+  projectId: "nextjs-database-60f90",
+  storageBucket: "nextjs-database-60f90.appspot.com",
+  messagingSenderId: "1034160308172",
+  appId: "1:1034160308172:web:b0ab71b8d789e939c76769"
+};
 
-  return {
+  initializeApp(firebaseConfig);
+
+  const db = getFirestore();
+  const colref = collection(db, 'technologies');
+  const q = query(colref, where('name', '==', dbParam));
+  const querySnapshot = await getDocs(q);
+  
+  let docData: any = [];
+  querySnapshot.forEach((doc) => {
+   docData.push({...doc.data()}) 
+  })
+
+    return {
     props: {
-      details: details,
+      dbData: JSON.stringify(docData) || []
     },
   };
 };
 
 interface pageProps {
-  details: {
-    name: string;
-    details: string;
-    avatar: string;
-  };
+  dbData: string
 }
 
 const bodyVariants: Variants = {
@@ -48,8 +57,10 @@ const bodyVariants: Variants = {
   },
 };
 
-const page = ({ details }: pageProps) => {
-  const { name, details: deepDetails, avatar } = details || {};
+const page = ({ dbData }: pageProps) => {
+  
+  const parsedData = JSON.parse(dbData)[0];
+  const { name, details: deepDetails, avatar } = parsedData || {};
 
   return (
     <div className={styles.detailsWrapper}>
